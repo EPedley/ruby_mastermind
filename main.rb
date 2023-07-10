b8b01af9617613812d3b4076e95ff76f8e9269ec
EPedley <37877423+EPedley@users.noreply.github.com>
2023-07-10 16:01:44 +0100

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
 require_relative 'game.rb'
 require_relative 'human_solver.rb'
-# require_relative 'computer_solver.rb'
+require_relative 'computer_solver.rb'
 require_relative 'logic.rb'
 require_relative 'text_content.rb'
 require_relative 'text_instructions.rb'
