712a0c38e8a9a3b89736f6b0d7c460e4bec390aa
EPedley <37877423+EPedley@users.noreply.github.com>
2023-07-10 16:01:04 +0100

    Update human_solver.rb

--- human_solver.rb
+++ human_solver.rb
@@ -1,36 +1,33 @@
 require_relative 'text_content.rb'
+require_relative 'logic.rb'
+require_relative 'display.rb'
 
 class HumanSolver
+
+  include TextContent
+  include Logic
+
+  def initialize
+    @code = ""    
+    @game_type = "human_solver"
+  end
+
+  def game
+    intro(@game_type)
+    @code = set_code
+    12.times {play_round}
+    game_lost(@game_type, @code)
+  end
   
   def set_code
     4.times.map{rand(1..6)}.join
   end
 
-end
-
-module HumanResponse
-
-  def user_prompt
-    guess = ""
-    loop do
-      guess = get_input
-      break if check_user_input(guess) == true
-    end
-  guess
-  end
-  
-  def check_user_input(guess)
-    if guess.length == 4 and guess.match(/^[1-6]{4}$/)
-      true
-    else
-      input_warning
-      false
-    end
-    # if guess.length == 4
-    #   true 
-    # else
-    #   false
-    # end
+  def play_round
+    guess = user_prompt
+    clues = compare_codes(@code, guess)
+    show_results(guess, clues)
+    if is_there_a_winner(@code, guess) then there_is_a_winner(@game_type) end  
   end
 
 end
