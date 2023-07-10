0760c9cc478af3ecd87654026614ef57663b7593
EPedley <37877423+EPedley@users.noreply.github.com>
2023-07-10 16:01:17 +0100

    Update logic.rb

--- logic.rb
+++ logic.rb
@@ -1,4 +1,15 @@
 module Logic
+
+  def set_game_type
+    game_type = gets.chomp
+    puts
+    until game_type == "1" or game_type == "2"
+      game_type_warning
+      game_type = gets.chomp
+    end
+    game_type == "1" ? "computer_solver" : "human_solver"
+  end
+  
   def compare_codes(maker_code, guess)
     arr = []
     
@@ -9,19 +20,21 @@ module Logic
     temp_guess_code = []
 
     maker_code.each_with_index do |x, i|
-      if x == guess_code[i]
+      if guess_code[i] == x      
         arr.push("exact_guess")
       else
-        temp_maker_code.push(x)
+        temp_maker_code.push(maker_code[i])
         temp_guess_code.push(guess_code[i])
       end
+
     end
 
-    temp_maker_code.each_with_index do |y, j|
-      if temp_guess_code.include?(y)
+    temp_guess_code.each_with_index do |x, i|
+      if temp_maker_code.include?(x)  
+        temp_maker_code.delete_at(temp_maker_code.find_index(x))
         arr.push("partial_guess")
       end
-    end
+    end  
     
     arr.sort
   end
@@ -33,4 +46,23 @@ module Logic
       false
     end
   end
+
+  def user_prompt
+    guess = ""
+    loop do
+      guess = get_code
+      break if code_check(guess) == true
+    end
+  guess
+  end
+
+  def code_check(code)
+    if code.length == 4 and code.match(/^[1-6]{4}$/)
+      true
+    else
+      code_warning
+      false
+    end
+  end
+  
 end
