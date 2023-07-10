624e9cd0056a4a8129f882a4f76e703d5b9db4ed
EPedley <37877423+EPedley@users.noreply.github.com>
2023-07-10 16:01:57 +0100

    Update text_content.rb

--- text_content.rb
+++ text_content.rb
@@ -22,24 +22,58 @@ module TextContent
     arr.join
   end
 
-  def there_is_a_winner
-    puts "#{"YOU GUESSED THE CODE CORRECTLY! YOU HAVE WON\n".bold.gold}"
-    exit
+  def there_is_a_winner(game_type)
+    if game_type == "human_solver"
+      puts "#{"YOU GUESSED THE CODE CORRECTLY! YOU HAVE WON\n".bold.gold}"
+      exit
+    else
+      puts "#{"THE COMPUTER SUCCESSFULLY GUESSED YOUR CODE!\n".bold.brown}"
+      exit
+    end
   end
 
-  def game_lost(code)
-    puts "#{"YOU DIDN'T MANAGE TO GUESS THE CODE CORRECTLY! IT WAS #{code}. YOU HAVE LOST.\n".bold.red}"
-    exit
+  def game_lost(game_type, code)
+    if game_type == "human_solver"
+      puts "#{"YOU DIDN'T MANAGE TO GUESS THE CODE CORRECTLY! IT WAS #{code}. YOU HAVE LOST.\n".bold.brown}"
+      exit
+    else
+      puts "#{"YOU WON! THE COMPUTER DIDN'T MANAGE TO GUESS YOUR CODE CORRECTLY!\n".bold.gold}"
+      exit
+    end
   end
 
-  def input_warning
+  def game_type_warning
+    puts "\n#{"That is not a valid game type. Please try again.".brown}"
+    puts
+  end
+
+  def code_warning
     puts "\n#{"That is not a valid four digit code. Please try again.".brown}"
     puts
   end
 
-  def get_input
+  def get_code
     puts "Please enter your four digit code below:"
     gets.chomp
   end
+
+  def intro(game_type)
+    if game_type == "human_solver"
+      puts "The computer has set a four digit code."
+      puts
+    end
+    if game_type == "computer_solver"
+      puts "Enter your four digit code below:"
+      code = gets.chomp
+      puts
+      return code
+    end
+  end
+
+  # def loading
+  #   sleep(1)
+  #   puts "#{"Loading...".grey}"
+  #   puts
+  # end
   
 end
